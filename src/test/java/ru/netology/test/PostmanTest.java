2c04127296e7a7462631ba3356bea5b59a66fdb5
Deportator <fabrichnov87@gmail.com>
2022-07-31 14:56:49 +0300

    Failing CI

---
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -0,0 +1,35 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanTest {
+
+    @Test
+    public void test() {
+// Given - When - Then
+// Предусловия
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=utf-8")
+                .body("тест") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .log().all()
+                .when()
+                .post("/post")
+
+// Проверки
+                .then()
+                .log().all()
+
+                .statusCode(200)
+                .header("content-type", "application/json; charset=utf-8")
+
+                .body("data.", is("falling тест"))
+                .body("data", equalTo("falling тест"))
+        ;
+    }
+}
